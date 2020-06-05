6cab04071c8c7a41ea31a3a8e4ca09f205d5362a
Sweta Kumari <28506362+swetakum@users.noreply.github.com>
2020-06-05 11:49:25 -0700

    Create packer.sh

---
+++ packer.sh
@@ -0,0 +1,6 @@
+cd /usr/local/bin/
+wget https://releases.hashicorp.com/packer/1.2.5/packer_1.2.5_linux_amd64.zip
+unzip packer_1.2.5_linux_amd64.zip
+rm -f packer_1.2.5_linux_amd64.zip
+cd
+packer --version
